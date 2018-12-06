7913a5cb91d762303cf86a76901e9a798508b207
willamb <willamb@umich.edu>
2018-12-06 14:42:17 -0500

    items loading

--- fashionbot/src/App.js
+++ fashionbot/src/App.js
@@ -5,7 +5,8 @@ import './App.css';
 class App extends Component {
 
   state = {
-    selectedFile: null
+    selectedFile: null,
+    results: []
   }
 
   fileSelectedHandler = event => {
@@ -22,9 +23,12 @@ class App extends Component {
       headers: {'Access-Control-Allow-Origin': '*'}
     };
 
-    axios.post('http://localhost:5000/query/M',fd, config)
+    axios.post('http://localhost:5000/query/M', fd, config)
       .then(res => {
         console.log(res)
+        this.setState({
+          results: res.data.results
+        })
       });
   }
 
@@ -33,6 +37,16 @@ class App extends Component {
       <div className="App">
         <input type="file" onChange={this.fileSelectedHandler}/>
         <button onClick={this.fileUploadHandler}>Upload</button>
+      {this.state.results.map(item =>
+        (<div>
+          <a href={item.url}>
+            <img src={item.images} alt="pic"/>
+          </a>
+          {item.price}
+          {item.product_brand}
+          {item.product_name}
+        </div>),
+      )}
       </div>
     );
   }
